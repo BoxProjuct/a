1a05445804aa554e3a7fb94722a2d56b037de62f
BoxBoxBoxBoxBox <110622134+BoxBoxBoxBoxBox@users.noreply.github.com>
2022-10-04 19:40:23 +0300

    Initial commit

---
+++ build.sh
@@ -0,0 +1,17 @@
+#sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/NFS-project/local_manifest --depth 1 -b rosy-derp-13 .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build rom
+export NFS_TARGET_BUILD_ROM=DerpFest
+source build/envsetup.sh
+lunch derp_rosy-userdebug
+export TZ=Asia/Jakarta
+export KBUILD_BUILD_USER=rosy
+export KBUILD_BUILD_HOST=nfsproject
+export BUILD_USERNAME=rosy
+export BUILD_HOSTNAME=nfsproject
+export USE_NFSparts=true
+mka derp -j8
+# end
